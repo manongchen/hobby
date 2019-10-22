fff538d3c0a98174a0fa2aaf360c4eb7474ced8d
xingchen <xingchen@miaodao.mobi>
2019-10-22 22:54:26 +0800

    新建user类

---
+++ src/main/java/com/manongc/hobby/master/entity/User.java
@@ -0,0 +1,13 @@
+package com.manongc.hobby.master.entity;
+
+/**
+ * @ClassName User
+ * @Description TODO()
+ * @Author xingchen
+ * @Date 2019-10-22 22:53
+ * @Version 1.0.0
+ */
+public class User {
+
+    private String name;
+}
