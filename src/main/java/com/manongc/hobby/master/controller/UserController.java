e4b08a144ad8fdb065b1449a56ba458b1f94b970
xingchen <xingchen@miaodao.mobi>
2019-10-22 23:38:19 +0800

    git stash 恢复测试

---
+++ src/main/java/com/manongc/hobby/master/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.manongc.hobby.master.controller;
+
+/**
+ * @ClassName UserController
+ * @Description TODO()
+ * @Author xingchen
+ * @Date 2019-10-22 23:23
+ * @Version 1.0.0
+ */
+public class UserController {
+}
---
+++ src/main/java/com/manongc/hobby/master/entity/Util.java
@@ -0,0 +1,12 @@
+package com.manongc.hobby.master.entity;
+
+/**
+ * @ClassName Util
+ * @Description TODO()
+ * @Author xingchen
+ * @Date 2019-10-22 23:21
+ * @Version 1.0.0
+ */
+public class Util {
+
+}
